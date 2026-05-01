f3cd0feabfadc557141116a06c2db5bfbe529689
Sudeep Cholkar <sudeepcholkar918@gmail.com>
2026-05-01 22:01:17 +0530

    mysql Installations

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "hello"
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+echo " $SCRIPT_NAME $TIMESTAMP $LOG_FILE"
