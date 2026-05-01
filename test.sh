5e011ec2c3c68dfd92b2d963aa8aead63032053a
Sudeep Cholkar <sudeepcholkar918@gmail.com>
2026-05-01 22:05:08 +0530

    mysql Installations

--- test.sh
+++ test.sh
@@ -5,4 +5,17 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 
-echo " $SCRIPT_NAME $TIMESTAMP $LOG_FILE"
+#echo "$SCRIPT_NAME $TIMESTAMP $LOG_FILE"
+
+USER_ID=$(id -u)
+
+CHECK_ROOT(){
+    if [ $USER_ID -ne 0 ]
+    then
+        echo "Please use sudo "
+    else
+        echo "running sudo "
+    fi
+}
+
+CHECK_ROOT
